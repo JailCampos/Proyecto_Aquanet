4fcd94e3c3072dfdf95ba84f399099d6e5795e49
JailCampos <jailantac@gmail.com>
2022-09-21 23:07:10 -0500

    Creacion_proyecto_y_conectar_base_datos

---
+++ ProyectoAquanet/src/main/java/prueba_conector/prueba.java
@@ -0,0 +1,14 @@
+package prueba_conector;
+
+import conexionbd.baseDatos;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		
+		@SuppressWarnings("unused")
+		baseDatos conexion = new baseDatos();
+
+	}
+	
+}
